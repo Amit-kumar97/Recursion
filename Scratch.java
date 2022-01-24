fa5090d699e641d84c3180092c6da5a19978c594
Amit-rajput97 <84862054+Amit-rajput97@users.noreply.github.com>
2022-01-25 00:04:13 +0530

    Update Scratch.java

--- Scratch.java
+++ Scratch.java
@@ -6,23 +6,26 @@ public class Scratch {
     // output= "neerj"
     // removing character a from the string
 
-    public static String remove(String str) {
+    public static String remove(String str,char ch1) {
         // base case
         if (str.length() == 0) {
             return str;
         }
         String result = "";
         // i think i mentioned == here insted of !=  in class
-        if (str.charAt(0) != 'a') {
+        if (str.charAt(0) != ch1) {
             result = result + str.charAt(0);
         }
         // recursive call
-        String recans = remove(str.substring(1));
+        String recans = remove(str.substring(1),ch1);
         return result + recans;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String name ="neeraj";
-        System.out.println(remove(name));
+        System.out.println("Enter a String:");
+        String name = sc.next();                          //---------"neeraj";
+        System.out.println("Enter a character to remove from String:");
+        char ch1 = sc.next().charAt(0);                   //----------'a'
+        System.out.println(remove(name,ch1));
     }
 }
