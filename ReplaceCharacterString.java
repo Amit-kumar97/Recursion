b3efee599ac2e88253f239240bdce78218011c04
Amit-rajput97 <84862054+Amit-rajput97@users.noreply.github.com>
2022-01-24 23:51:16 +0530

    Update ReplaceCharacterString.java

--- ReplaceCharacterString.java
+++ ReplaceCharacterString.java
@@ -17,10 +17,10 @@ public class ReplaceCharacterString {
         System.out.println("Enter a String:");
         String str = sc.next();                                     //-----------"abaca";
         System.out.println("Enter a character that you want to replace from the given String:");
-        char ch1 = sc.next().charAt(0);                            //--------'a';
+        char ch1 = sc.next().charAt(0);                             //------------'a';
         System.out.println("Enter a character from which you want to replace given character from the String:");
         char ch2 = sc.next().charAt(0);                             //-------------'x';
-        System.out.println(replace_char(str,ch1,ch2));
+        System.out.println("New String:"+replace_char(str,ch1,ch2));
     }
     //------------method here to implement the logic
     public static String replace_char(String str, char ch1,char ch2)
@@ -30,16 +30,16 @@ public class ReplaceCharacterString {
         {
             return str;
         }
-        String result = "";                                    //--------creating a local empty string
-        if(str.charAt(0)==ch1)                                 //--------if character matches than it will replace with ch2
+        String result = "";                                    //------------creating a local empty string
+        if(str.charAt(0)==ch1)                                 //------------if character matches than it will replace with ch2
         {
             result = result + ch2;
         }
-        else                                                   //---------if character not matches than it will append same character
+        else                                                   //------------if character not matches than it will append same character
         {
             result = result + str.charAt(0);
         }
-        String res_ans = replace_char(str.substring(1),ch1,ch2);              //--------recursion call
+        String res_ans = replace_char(str.substring(1),ch1,ch2);              //--------Recursion call
         return result+res_ans;
     }
 }
