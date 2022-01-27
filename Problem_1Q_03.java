9b6c66824401ba9c478ac933099597a6f89909be
Amit-rajput97 <84862054+Amit-rajput97@users.noreply.github.com>
2022-01-27 23:47:35 +0530

    Create Problem_1Q_03.java
    Finding Error in the given  code Snippet.(Recursion)

---
+++ Problem_1Q_03.java
@@ -0,0 +1,24 @@
+//--------------Code Snippet
+
+public class Problem_1Q_03 {
+    public static void print(int n)
+    {
+        if(n>0)
+        {
+            return;
+        }
+        if(n==0)
+        {
+            System.out.println(n);
+            return;
+        }
+        System.out.println(n+"");
+        n--;
+        print(n);
+    }
+
+    public static void main(String[] args) {
+        int num = 5;
+        print(num);
+    }
+}
