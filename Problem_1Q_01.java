046055f101e721d89ce44e618f8409e8a70d9997
Amit-rajput97 <84862054+Amit-rajput97@users.noreply.github.com>
2022-01-27 23:41:38 +0530

    Create Problem_1Q_01.java
    Finding Errors in the given code Snippet.(Recursion)

---
+++ Problem_1Q_01.java
@@ -0,0 +1,14 @@
+//------------Code Snippet
+
+public class Problem_1Q_01 {
+    public static  int function(int num)
+    {
+        return function(0);
+    }
+
+    public static void main(String[] args) {
+        int num = 5;
+        int ans = function(num-1);
+        System.out.println(ans);
+    }
+}
