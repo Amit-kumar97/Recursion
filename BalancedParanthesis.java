4c7e86bff4376b3d5ca7f9356c30099f4f3619ca
Amit-rajput97 <84862054+Amit-rajput97@users.noreply.github.com>
2022-01-28 22:20:58 +0530

    Update BalancedParanthesis.java
    check all type of possible pair of parenthesis.(recursion)

--- BalancedParanthesis.java
+++ BalancedParanthesis.java
@@ -1,17 +1,17 @@
-
 //---------------Checks if the expression is balanced or not,Do it using recursion.
+
 public class BalancedParanthesis
 {
-        // Function that print all combinations of
-        // balanced parentheses
-        // open store the count of opening parenthesis
-        // close store the count of closing parenthesis
-        static void _printParenthesis(char str[], int pos,
+        //---------- Function that print all combinations of
+        //---------- balanced parentheses
+        //---------- open store the count of opening parenthesis
+        //---------- close store the count of closing parenthesis
+        static void printParenthesis(char str[], int pos,
         int n, int open,
         int close)
         {
             if (close == n) {
-                // print the possible combinations
+                //------------ print the possible combinations
                 for (int i = 0; i < str.length; i++)
                     System.out.print(str[i]);
                 System.out.println();
@@ -20,22 +20,22 @@ public class BalancedParanthesis
             else {
                 if (open > close) {
                     str[pos] = '}';
-                    _printParenthesis(str, pos + 1, n, open,
+                    printParenthesis(str, pos + 1, n, open,
                             close + 1);
                 }
                 if (open < n) {
                     str[pos] = '{';
-                    _printParenthesis(str, pos + 1, n, open + 1,
+                    printParenthesis(str, pos + 1, n, open + 1,
                             close);
                 }
             }
         }
 
-        // Wrapper over _printParenthesis()
+        //------------- Wrapper over printParenthesis()
         static void printParenthesis(char str[], int n)
         {
             if (n > 0)
-                _printParenthesis(str, 0, n, 0, 0);
+                printParenthesis(str, 0, n, 0, 0);
             return;
         }
 
